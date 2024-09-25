3022652eaf7deaae1e2a2aeabbe27c05075d462a
shashikant Borude <119213408+shashikant62@users.noreply.github.com>
2024-09-25 20:41:51 +0530

    Update socketconnection.js

--- client/src/components/socketconnection.js
+++ client/src/components/socketconnection.js
@@ -1,6 +1,6 @@
 import {io} from 'socket.io-client';
 export const initsocket=async()=>{
-    const backent_url="http://localhost:5000";
+    const backent_url="https://codehelp-backend-4ivg.onrender.com";
     const options={
         'force new connection': true,
         reconnectionAttempt:'Infinity',
